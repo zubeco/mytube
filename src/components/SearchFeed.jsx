8c70c16483c86c78dc967e536b056213046fc2cc
udeh princewill <princezubby2020@gmail.com>
2023-01-02 10:02:28 +0100

    first commit

---
+++ src/components/SearchFeed.jsx
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react";
+import { Box, Typography } from "@mui/material";
+import { borderRight } from "@mui/system";
+import { Videos } from "./";
+import { useParams } from "react-router-dom";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
+
+export default function SearchFeed() {
+  const [videos, setVideos] = useState([]);
+  const { searchTerm } = useParams();
+
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${searchTerm}`)
+      .then((data) => setVideos(data.items))
+      .catch((err) => console.log(err));
+  }, [searchTerm]);
+
+
+  return (
+    <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+      <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
+        Search Results for:{" "}
+        <span style={{ color: "#FC1503" }}>{searchTerm}</span>
+      </Typography>
+      videos
+      <Videos videos={videos} />
+    </Box>
+  );
+}
